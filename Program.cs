991ca92e276dbfee3a7cdff1cea0032d635e06bd
“codewitherrors” <codewitherrors1@gmail.com>
2023-02-10 12:49:04 +0530

    Add : Displaying Welcome to Line Comparison Computation Program

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LineComparisionProblem
+{
+    public class LineComparision
+    { 
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LengthOfTwoPoint.lengthOfTwoPoint();
+        }
+    }
+}
